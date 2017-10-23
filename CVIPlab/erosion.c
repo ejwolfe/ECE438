4f9b82f134709b21e1c5727a5f042585b61b9a65
Eric Wolfe <rojowolf21@gmail.com>
2017-10-22 19:46:14 -0500

    Working on lab 6

--- CVIPlab/erosion.c
+++ CVIPlab/erosion.c
@@ -8,9 +8,7 @@
 #include "CVIPimage.h"
 #include "CVIPlab.h"
 
-byte runOr(int a, int b);
-
-Image *dilation(Image *inputImage, byte **structuringElement){
+Image *erosion(Image *inputImage, byte **structuringElement){
     Image *outputImage;
 	byte **inputData;
     byte **outputData;
@@ -24,15 +22,15 @@ Image *dilation(Image *inputImage, byte **structuringElement){
         for (int r = 1; r < numberRows-1; r++){
             for (int c = 1; c < numberCols-1; c++){
                 if(inputData[r][c] != 0){
-                    outputData[r-1][c-1] = runOr(inputData[r-1][c-1], structuringElement[0][0]);
-                    outputData[r-1][c] = runOr(inputData[r-1][c], structuringElement[0][1]);
-                    outputData[r-1][c+1] = runOr(inputData[r-1][c+1], structuringElement[0][2]);
-                    outputData[r][c-1] = runOr(inputData[r][c-1], structuringElement[1][0]);
-                    outputData[r][c] = runOr(inputData[r][c], structuringElement[1][1]);
-                    outputData[r][c+1] = runOr(inputData[r][c+1], structuringElement[1][2]);
-                    outputData[r+1][c-1] = runOr(inputData[r+1][c-1], structuringElement[2][0]);
-                    outputData[r+1][c] = runOr(inputData[r+1][c], structuringElement[2][1]);
-                    outputData[r+1][c+1] = runOr(inputData[r+1][c+1], structuringElement[2][2]);
+                    outputData[r-1][c-1] = (inputData[r-1][c-1] == 1 && structuringElement[0][0] == 1) ? 0 : inputData[r-1][c-1];
+                    outputData[r-1][c] = (inputData[r-1][c] == 1 && structuringElement[0][1] == 1) ? 0 : inputData[r-1][c];
+                    outputData[r-1][c+1] = (inputData[r-1][c+1] == 1 && structuringElement[0][2] == 1) ? 0 : inputData[r-1][c+1];
+                    outputData[r][c-1] = (inputData[r][c-1] == 1 && structuringElement[1][0] == 1) ? 0 : inputData[r][c-1];
+                    outputData[r][c] = 255;
+                    outputData[r][c+1] = (inputData[r][c+1] == 1 && structuringElement[1][2] == 1) ? 0 : inputData[r][c+1];
+                    outputData[r+1][c-1] = (inputData[r+1][c-1] == 1 && structuringElement[2][0] == 1) ? 0 : inputData[r+1][c-1];
+                    outputData[r+1][c] = (inputData[r+1][c] == 1 && structuringElement[2][1] == 1) ? 0 : inputData[r+1][c];
+                    outputData[r+1][c+1] = (inputData[r+1][c+1] == 1 && structuringElement[2][2] == 1) ? 0 : inputData[r+1][c+1];
                 }
             }
         }
@@ -48,11 +46,4 @@ Image *dilation(Image *inputImage, byte **structuringElement){
     outputImage = remap_Image(outputImage, CVIP_BYTE, 0, 255);
 
     return outputImage;
-}
-
-byte runOr(byte a, byte b){
-    if(a == 0 && b == 0){
-        return 0;
-    }
-    return 255;
 }
